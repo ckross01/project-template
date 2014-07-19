a9110c6c92ae9c573be9faddfc51150ce8cfd770
ckross01 <christopher.k.ross@gmail.com>
2014-07-19 13:53:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+project-template
+=============
+
+A basic setup for MEAN stack projects.
+A few things are decided, 
+gulpjs,
+mocha, 
+jade,
+bower
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+gulp.task('lint', function() {
+    return gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/*.js')
+        .pipe(rename('simplecookie.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/*.js', ['lint', 'scripts']);
+});
+
+gulp.task('default', ['lint', 'scripts', 'watch']);
